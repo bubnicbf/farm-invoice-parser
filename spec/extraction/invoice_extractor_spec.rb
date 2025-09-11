64cd5711e99e69e86fb227dbcb0a5e71eeef16b3
Ben Bubnick <ben.bubnick@gmail.com>
2025-09-11 17:07:53 -0400

    add initial spec to verify invoice extractor interface

---
+++ spec/extraction/invoice_extractor_spec.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+require "json"
+require_relative "../../lib/extraction/invoice_extractor"
+
+RSpec.describe Extraction::InvoiceExtractor do
+  it "responds to extract" do
+    extractor = described_class.new
+    expect(extractor).to respond_to(:extract)
+  end
+end
