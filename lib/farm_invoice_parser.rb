77d660c89a25f79c21e27f2ccd3fab15f2787bee
Ben Bubnick <ben.bubnick@gmail.com>
2025-09-11 17:12:23 -0400

    add base FarmInvoiceParser module with version constant and hello method

---
+++ lib/farm_invoice_parser.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require "json"
+require "oj"
+require "dotenv/load"
+
+module FarmInvoiceParser
+  VERSION = "0.1.0"
+
+  def self.hello = "Farm Invoice Parser v#{VERSION}"
+end
